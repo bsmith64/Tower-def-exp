e56db7b7c6b6d2d8eafb5f4596292032ee626861
Blake Smith <blake@skona.com>
2017-04-29 21:21:14 -0700

    Map revamp - callback needs help

--- js/game/enemies.js
+++ js/game/enemies.js
@@ -4,7 +4,7 @@
 	1. RG_ == Rendering in Game
 */
 
-var spawnBox = document.getElementById('unit-spawn');
+var spawnBox = document.getElementById('cell-202');
 
 // Pop defines the number of units sent
 var pop = 10;
@@ -25,9 +25,21 @@ for (var i = 0; i < pop; i++) {
 }
 
 // Spawns enemies
-for (var s = 0; s < spawnCollection.length; s++) {
-	var RG_enemy = document.createElement('div');
-	RG_enemy.className = 'enemy-unit';
-
-	spawnBox.appendChild(RG_enemy);
+function spawn(s, sendUnit) {
+	for (var s = 0; s < spawnCollection.length; s++) {
+		var RG_enemy = document.createElement('div');
+		RG_enemy.className = 'enemy-unit';
+		spawnBox.appendChild(RG_enemy);
+	}
+	sendUnit();
 }
+
+// callback needs help !!!!!!
+function sendUnit() {
+	setTimeout(function() {
+		RG_enemy.style.left = "100px";
+		console.log('sending');
+	}, 200);
+}
+
+spawn();
--- js/game/gameboard.js
+++ js/game/gameboard.js
@@ -1,15 +1,19 @@
 // Draw Gameboard
 
 var lastClicked;
-var grid = clickableGrid(10,20,function(el,row,col,i){
+
+var grid = clickableGrid(11,20,function(el,row,col,i){
+
     console.log("You clicked on element:",el);
     console.log("You clicked on row:",row);
     console.log("You clicked on col:",col);
     console.log("You clicked on item #:",i);
 
     el.className='clicked';
+
     if (lastClicked) lastClicked.className='';
     lastClicked = el;
+
 });
 
 document.body.appendChild(grid);
@@ -18,11 +22,13 @@ function clickableGrid( rows, cols, callback ){
     var i=0;
     var grid = document.createElement('table');
     grid.className = 'grid';
+    grid.id = 'gameBoard';
     for (var r=0; r < rows; r++) {
         var tr = grid.appendChild(document.createElement('tr'));
         for (var c=0; c<cols; c++) {
             var cell = tr.appendChild(document.createElement('td'));
             cell.innerHTML = ++i;
+            cell.id = "cell-" + ++i;
             cell.addEventListener('click',(function(el,r,c,i){
                 return function(){
                     callback(el,r,c,i);
@@ -32,3 +38,33 @@ function clickableGrid( rows, cols, callback ){
     }
     return grid;
 }
+
+
+
+var gb_width = document.getElementById('gameBoard').offsetWidth;
+var gb_height = document.getElementById('gameBoard').offsetheight;
+
+console.log('width: ' + gb_width);
+console.log('height: ' + gb_height);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
